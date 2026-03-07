77d670daa580a84ad8afd9deccedee3ca5f4d61d
Ashish Sharma <stackdev.ash@gmail.com>
2026-03-07 22:26:03 +0530

    Initial TypeScript setup and first TS file

---
+++ src/first.ts
@@ -0,0 +1,7 @@
+function welcome(name:string, date:Date) {
+     console.log(`Learning Typescript from ${name} on date ${date.toDateString()}!`);
+}
+
+welcome("Chai aur Code , typescriptlang.org",new Date());
+
+// code is mostly similar to js , in ts types are the main deal we can use : to declare which type of data variable and functions will use and return 
